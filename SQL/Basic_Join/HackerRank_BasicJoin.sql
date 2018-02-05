1f9d0acfc05d4ad567681137a47b985124445582
fracchan <fracchan@gmail.com>
2018-02-05 13:21:02 -0800

    Added the "Top Competitor" solution

--- SQL/Basic_Join/HackerRank_BasicJoin.sql
+++ SQL/Basic_Join/HackerRank_BasicJoin.sql
@@ -26,3 +26,25 @@ from students st
 join grades gr on st.marks between gr.min_mark and gr.max_mark
 order by gr.grade desc, st.name
 ;
+
+-- Top Competitors
+/*
+To Avoid the following error message, need to group by all the columns in the select
+statement. This message can or cannot appear base on the mysql options chosen. 
+
+ERROR 1055 (42000) at line 1: Expression #2 of SELECT list is not in GROUP BY 
+  clause and contains nonaggregated column 'run_0iw2oz7eqxy.ha.name' 
+  which is not functionally dependent on columns in GROUP BY clause; 
+  this is incompatible with sql_mode=only_full_group_by
+*/
+select ha.hacker_id, ha.name
+from hackers ha
+join submissions su using (hacker_id)
+join challenges ch using (challenge_id)
+join difficulty di using (difficulty_level)
+where su.score = di.score
+group by ha.hacker_id, ha.name
+having count(ch.challenge_id) > 1
+order by count(ch.challenge_id) desc, ha.hacker_id
+;
+
